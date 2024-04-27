18205db03c4b66ba9178061bb11112e9a867d901
amrhakim <60184135+amrhakim@users.noreply.github.com>
2024-04-27 14:02:23 +0300

    first update

--- Config.js
+++ Config.js
@@ -3,13 +3,13 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = [ process.env.OWNER_NUMBER || "+966561490313"],
+global.ownername = process.env.OWNER_NAME || "Tajwwal",
+global.ytname = "YT: Tajwwal"
+global.socialm = "GitHub: Amrhakim"
+global.location = "Egypt"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "Tajawwal",//name of the bot
 
 //sticker details
 global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
